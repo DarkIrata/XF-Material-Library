41a694a73fb820c840dcd4089c82ec4a5a860f47
DarkIrata <git@ipmix.de>
2022-08-09 00:19:10 +0200

    Tests used for MaterialTextFieldView

--- Samples/MaterialMvvmSample/Views/MaterialTextFieldView.xaml.cs
+++ Samples/MaterialMvvmSample/Views/MaterialTextFieldView.xaml.cs
@@ -10,5 +10,36 @@ namespace MaterialMvvmSample.Views
             InitializeComponent();
             BindingContext = new MaterialTextFieldViewModel();
         }
+
+        private void TestBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (e.NewTextValue == "aa")
+            {
+                this.TestBox.ErrorText = "OH NOO";
+                this.TestBox.HelperText = "OH NOO";
+            }
+            else
+            {
+                this.TestBox.ErrorText = null;
+                this.TestBox.HelperText = null;
+            }
+
+            this.TestBox.HasError = !string.IsNullOrEmpty(this.TestBox.ErrorText);
+        }
+
+        private void TestButton_Clicked(object sender, System.EventArgs e)
+        {
+            this.TestBox.IsEnabled = !this.TestBox.IsEnabled;
+        }
+
+        private void Test2Button_Clicked(object sender, System.EventArgs e)
+        {
+            this.TestBox.Text += "aa";
+        }
+
+        private void TapGestureRecognizer_Tapped(object sender, System.EventArgs e)
+        {
+            this.TestBox.Text += "aab";
+        }
     }
 }
