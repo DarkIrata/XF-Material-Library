b600decb436f8838a6807d11dc2c5cde0bcc1fd3
DarkIrata <git@ipmix.de>
2022-08-09 22:18:49 +0200

    More MaterialSwitch tests

--- Samples/MaterialMvvmSample/ViewModels/MaterialSwitchViewModel.cs
+++ Samples/MaterialMvvmSample/ViewModels/MaterialSwitchViewModel.cs
@@ -1,9 +1,23 @@
-﻿namespace MaterialMvvmSample.ViewModels
+﻿using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace MaterialMvvmSample.ViewModels
 {
-    public class MaterialSwitchViewModel
+    public class MaterialSwitchViewModel : BaseViewModel
     {
+        private bool isActive = true;
+
+        public bool IsActive
+        {
+            get => this.isActive;
+            set => this.Set(ref this.isActive, value, nameof(this.IsActive));
+        }
+
+        public ICommand SwitchIsActiveCommand { get; }
+
         public MaterialSwitchViewModel()
         {
+            this.SwitchIsActiveCommand = new Command(() => this.IsActive = !this.IsActive);
         }
     }
 }
